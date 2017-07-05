3b387ac7e033c5499a1d40365aad080ddd14a45f
pocockn <pocockn@hotmail.co.uk>
2017-07-05 15:36:12 +0100

    Add standard Intellij git ignore, change from require to import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import "./App.css";
 import axios from "axios";
-let monzoAuth = require('./config');
+import monzoAuth from './config';
 
 class App extends Component {
 
